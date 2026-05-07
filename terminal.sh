9103da01be3d67d256d98b16cb5d92dd3e0e8573
abhikatta <abhinaykatta97@gmail.com>
2026-05-07 20:23:01 +0530

    feat: add terminal.sh script with commented examples of shell scripting features

---
+++ terminal.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash 
+
+
+
+# x=5
+# y=3
+# z=$x+$y
+# echo $((z))
+
+
+
+
+# x=5
+# y=3
+# z=$x-$y
+# echo $((z))
+
+
+
+# x=5
+# y=3
+# z="$x*$y"
+# echo $((z))
+
+
+# x=5
+# y=3
+# z=$x/$y
+# echo $((z))
+
+
+# x=5
+# y=3
+# z="$x%$y"
+# echo $((z))
+
+
+
+# greet(){
+#     echo "$2" 
+# }
+
+
+# greet "$4" "$1 extra"
+
+
+# echo arg count and all args $# "$@"
+
+
+# while [ $# -gt 0 ]; do
+#     echo "$1"
+#     shift
+# done
+
+
+
+# if [ "$1" -gt 60 ]; then
+#     echo "invalid seconds"
+#     exit 1
+
+# elif [ "$1" -le 60 ]; then
+# echo 'valid seconds'
+#      exit 0
+
+# fi
+
+
