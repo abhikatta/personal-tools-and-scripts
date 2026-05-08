5546cb7628a5c4ec49f262f2f0cb69bf0d10c5c4
abhikatta <abhinaykatta97@gmail.com>
2026-05-08 10:58:50 +0530

    feat: implement get_seconds function to parse time duration flags into total seconds

--- terminal.sh
+++ terminal.sh
@@ -65,3 +65,67 @@
 # fi
 
 
+# count=0 
+
+# while [ $# -gt 0 ]; do
+#     echo "$1" "$4"
+#     shift
+#     count=$((count+1))
+#     echo "-----"
+# done
+
+# echo $count
+
+
+# -m 10 
+# -s 10
+# -h 10
+
+
+
+help_message(){
+    echo "Help message placeholder"
+    return 0
+
+}
+
+
+
+get_seconds(){
+    result=0
+    while [ $# -gt 0 ]; do
+        if [ "$1" != --help ] &&  [ -z "$2" ];then
+                echo "Missing arg -h -m -s"
+                return 1
+        fi
+        case "$1" in
+            -s) 
+                result=$((result+$2))
+                ;;
+            -m) 
+                result=$((result+($2*60)))
+                ;;
+            -h) 
+                result=$((result+($2*60*60)))
+                ;;
+            -d)
+                result=$((result+($2*24*60*60)))
+                ;;
+            --help)
+                help_message
+                return 0
+                ;;
+            *) 
+                echo "invalid" 
+                help_message
+                return 1
+       
+        esac
+             
+        shift 2
+    done
+    echo "$result"
+    return 0
+}
+
+get_seconds "$@"
